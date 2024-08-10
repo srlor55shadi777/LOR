7a22a12b7927078a66694fcab0e53456cfc799ff
srlor55shadi777 <srlorhitashi@gmail.com>
2024-08-10 04:29:36 +0300

    Create C1.js

---
+++ C1.js
@@ -0,0 +1,28 @@
+let currentPage = 1;
+
+const pages = document.querySelectorAll('.page');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+prevBtn.addEventListener('click', () => {
+    if (currentPage > 1) {
+        pages[currentPage - 2].style.transform = 'rotateY(0deg)';
+        currentPage--;
+    }
+    updateButtons();
+});
+
+nextBtn.addEventListener('click', () => {
+    if (currentPage < pages.length) {
+        pages[currentPage - 1].style.transform = 'rotateY(-180deg)';
+        currentPage++;
+    }
+    updateButtons();
+});
+
+function updateButtons() {
+    prevBtn.classList.toggle('hidden', currentPage === 1);
+    nextBtn.classList.toggle('hidden', currentPage === pages.length);
+}
+
+updateButtons();
